90d513fab7476fc2027fb55e4e8a8b130f31261b
romul <rhernandez@fie.undef.edu.ar>
2024-09-23 11:23:41 -0300

    queda: hacer las subclases Conferencia magistral y Taller, agregar asistentes, conferencistas y eventos de ejemplo en el main, y usar la funcion removerItem del gestor para agregar dinamismo, embellecer con los getline.

---
+++ cmake-build-debug/CMakeFiles/TpPP32024.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TpPP32024.dir/main.cpp.obj"
+  "CMakeFiles/TpPP32024.dir/main.cpp.obj.d"
+  "TpPP32024.exe"
+  "TpPP32024.exe.manifest"
+  "TpPP32024.pdb"
+  "libTpPP32024.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TpPP32024.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
